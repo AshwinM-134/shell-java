ed7d36f5dc6b85bbb71ee307274a773830cc5c4f
Ashwin <ashwin.m134@gmail.com>
2026-05-08 18:40:20 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,13 +10,27 @@ public class Main {
                 break;
             }
             String input = sc.nextLine();
-            if (input.equals("exit")) {
+            String command = input.split(" ", 2)[0];
+
+            if (command.equals("exit")) {
                 break;
-            } else if (input.startsWith("echo ")) {
-                System.out.println(input.substring(5));
+            } else if (command.equals("echo")) {
+                if (input.length() > 4) {
+                    System.out.println(input.substring(5));
+                } else {
+                    System.out.println();
+                }
+                continue;
+            } else if (command.equals("type")) {
+                String commandToCheck = input.length() > 5 ? input.substring(5) : "";
+                if (commandToCheck.equals("echo") || commandToCheck.equals("exit") || commandToCheck.equals("type")) {
+                    System.out.println(commandToCheck + " is a shell builtin");
+                } else {
+                    System.out.println(commandToCheck + ": not found");
+                }
                 continue;
             }
-            System.out.println(input + ": command not found");
+            System.out.println(command + ": command not found");
         }
     }
 }
